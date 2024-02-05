7f69c832496c31126c4758eddf84070dcbe464f5
Igor Macevic <informatikdrive@gmail.com>
2024-02-05 18:38:02 +0800

    Added more Config

--- src/OpenAIConfig.php
+++ src/OpenAIConfig.php
@@ -6,10 +6,21 @@ use OpenAI\Client;
 
 class OpenAIConfig
 {
-    #test
     public string $apiKey;
 
     public ?Client $client = null;
 
     public string $model;
+
+    public ?string $temperature = null;
+
+    public ?int $maxTokens = null;
+
+    public ?float $topP = null;
+
+    public ?float $frequencyPenalty = null;
+
+    public ?float $presencePenalty = null;
+
+    public ?string $stop = null;
 }
